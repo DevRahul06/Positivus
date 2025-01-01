4b87f8b540893f531ad7c4070befe2a48acf7435
Rahul <rahulchavda6000@gmail.com>
2025-01-01 16:22:50 +0530

    Make Change in Slider

--- assets/Slider/Slick-slider.js
+++ assets/Slider/Slick-slider.js
@@ -4,9 +4,32 @@ $(document).ready(function () {
     slidesToShow: 5,
     autoplay: true, // Enable autoplay
     autoplaySpeed: 0, // Speed of autoplay (in ms)
-    speed:1000,
+    speed: 3000,
     arrows: false, // Show navigation arrows
     dots: false, // Show dots navigation
-    cssEase: 'linear',
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
